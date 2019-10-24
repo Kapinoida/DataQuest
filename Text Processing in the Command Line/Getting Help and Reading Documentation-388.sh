7751068597b8606e1b6eb06710ac9e4c80c4a94f
David Plaskett <dcplaskett@gmail.com>
2019-10-24 14:15:08 -0500

    New course added

---
+++ Text Processing in the Command Line/Getting Help and Reading Documentation-388.sh
@@ -0,0 +1,27 @@
+## 2. What type is this? ##
+
+/home/dq$ answer builtin file builtin keyword alias function
+
+## 3. Aliases, Functions and Keywords ##
+
+/home/dq$ compgen -k
+
+## 4. Knowing What You're Running ##
+
+/home/dq$ python -c "print(3/2)"
+
+## 5. Help, man! ##
+
+/home/dq$ man bash
+
+## 6. Built-in Help ##
+
+/home/dq$ help help
+
+## 7. How to Read Synopses ##
+
+/home/dq$ help id
+
+## 8. Less is More ##
+
+/home/dq/rg_data$ less -S Arts Education
---
+++ Text Processing in the Command Line/Redirection and Pipelines-391.sh
@@ -0,0 +1,31 @@
+## 1. Printing User Input ##
+
+/home/dq$ echo "This is a command line interface."
+
+## 2. Redirecting Output with > ##
+
+/home/dq$ help echo >/home/dq/echo_help
+
+## 3. Redirecting Output with >> ##
+
+/home/dq$ ls rg_ddata
+
+## 4. Creating Empty Files ##
+
+/home/dq$ touch file1 file2
+
+## 5. Why Pipelines? ##
+
+/home/dq$ cat math_dataset
+
+## 6. Using Pipelines ##
+
+/home/dq$ zen | grep better
+
+## 7. The Unix Philosophy ##
+
+/home/dq/rg_data$ ls -l | sort -u * | wc -l
+
+## 8. Trying to Redirect Errors ##
+
+/home/dq/rg_data$ echo "nothing" >/dev/null
