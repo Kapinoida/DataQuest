40155f69f834673525975e76fab79867188b7ed9
David Plaskett <dcplaskett@gmail.com>
2019-10-24 09:47:19 -0500

    Added new course

---
+++ Elements of the Command Line/The Filesystem-384.sh
@@ -0,0 +1,27 @@
+## 1. The Filesystem ##
+
+/home$ answer false true true
+
+## 2. Working Directory ##
+
+/home$ pwd
+
+## 3. Inspecting Directories ##
+
+/home$ ls -p
+
+## 4. Inspecting Directories Thoroughly ##
+
+/home$ ls /dq/prize_winners
+
+## 5. Navigating the Filesystem ##
+
+/home$ cd /home/dq/prize_winners
+
+## 6. Absolute and Relative Paths ##
+
+/home$ ls -al dq/prize_winners
+
+## 7. Shortcuts for Filesystem Navigation ##
+
+/home$ cd dq/prize_winners
